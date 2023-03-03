299133d6dfa2142b4911ce2a272937f1ee42573c
Ryan Peabody <rpeabody43@gmail.com>
2023-03-03 12:12:05 -0500

    awesome sprites

--- src/Sketch.java
+++ src/Sketch.java
@@ -1,8 +1,11 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 
     private Chess chess;
+    private PImage[] whitePieces;
+    private PImage[] blackPieces;
 
     public static void main (String[] args) {
         String[] processingArgs = {"ChessAI"};
@@ -19,21 +22,46 @@ public class Sketch extends PApplet {
         size(512, 512);
     }
 
+    @Override
+    public void setup () {
+        String whitePath = "sprites/ChessPiecesv1/White/";
+        whitePieces = new PImage[6];
+        String blackPath = "sprites/ChessPiecesv1/Black/";
+        blackPieces = new PImage[6];
+        for (int i = 1; i <= 6; i++) {
+            String piece = switch (i) {
+                case Board.P:
+                    yield "pawn";
+                case Board.N:
+                    yield "knight";
+                case Board.B:
+                    yield "bishop";
+                case Board.R:
+                    yield "rook";
+                case Board.Q:
+                    yield "queen";
+                case Board.K:
+                    yield "king";
+                default:
+                    yield "";
+            };
+            whitePieces[i-1] = loadImage( "sprites/ChessPiecesv1/White/white"+piece+"_v1.png");
+            blackPieces[i-1] = loadImage( "sprites/ChessPiecesv1/Black/black"+piece+"_v1.png");
+        }
+
+    }
+
     private void drawBoard () {
         // BACKGROUND
         fill(20, 100, 135);
         noStroke();
         background(237, 226, 199);
         float squareWidth = width / 8f;
+        int[][] currentBoard = chess.currentBoard().pieces;
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if ((i + j) % 2 == 1)
-                    square(squareWidth * i, squareWidth * j, squareWidth);
-            }
-        }
-        int[][] currentBoard = chess.currentBoard().pieces;
-        for (int i = 0; i < 8; i++)
-            for (int j = 0; j < 8; j++) {
+                    square(squareWidth * j, squareWidth * i, squareWidth);
                 int piece = currentBoard[i][j];
                 char letter = switch (Math.abs(piece)) {
                     case Board.P: yield 'P';
@@ -44,12 +72,13 @@ public class Sketch extends PApplet {
                     case Board.K: yield 'K';
                     default: yield ' ';
                 };
-                stroke(0);
-                textAlign(CENTER, CENTER);
-                fill(piece > 0 ? 255 : 0);
-                textSize(25);
-                text(letter, squareWidth*j + squareWidth/2, squareWidth*i + squareWidth/2);
+                if (piece != 0) {
+                    int absPiece = Math.abs(piece);
+                    PImage sprite = piece > 0 ? whitePieces[absPiece - 1] : blackPieces[absPiece - 1];
+                    image(sprite, squareWidth * j, squareWidth * i, squareWidth, squareWidth);
+                }
             }
+        }
     }
 
     // -- GAMELOOP --
