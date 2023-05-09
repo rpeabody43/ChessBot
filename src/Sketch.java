fed9601499bbb9e24fbe409c34afd350ed43bece
Ryan Peabody <rpeabody43@gmail.com>
2023-05-08 22:27:37 -0400

    stop printing selectedSquare

--- src/Sketch.java
+++ src/Sketch.java
@@ -111,8 +111,6 @@ public class Sketch extends PApplet {
             possibleMoves = possibleMovesAtSelectedSq();
         } else possibleMoves = new HashMap<>();
 
-        System.out.println(selectedSquare);
-
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if ((i + j) % 2 == 1) {
